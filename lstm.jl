5f6c4cdd4efb2c837288251c77304c7846b266d1
kaandemiroz <OKD@OKDMac.local>
2016-04-28 10:19:09 +0300

    small fixes

--- lstm.jl
+++ lstm.jl
@@ -1,19 +1,19 @@
 using Knet
 using JLD
 
-@knet function lstmnetwork(word; nlayer = 2, size = 1000, vocab_size = 12594, lstm_type = :lstm1, o...)
+@knet function lstm_network(word; nlayer = 2, size = 1000, vocab_size = 12594, o...)
 	wvec = wdot(word; out = size, init = Uniform(-0.08, 0.08), o...)
-    y = repeat(wvec; o..., frepeat = lstm_type, nrepeat = nlayer, out = size)
+    y = repeat(wvec; o..., frepeat = :lstm_unit, nrepeat = nlayer, out = size)
     return wbf(y; o..., out = vocab_size, f = :soft)
 end
 
-@knet function lstm1(x; o...)
-	return lstm(x; out = 1000, winit = Uniform(-0.08, 0.08), binit = Constant(0), fbias = 0, o...)
+@knet function lstm_unit(x; o...)
+	return lstm(x; winit = Uniform(-0.08, 0.08), binit = Constant(0), fbias = 0, o...)
 end
 
 # Compile the LSTM Network to lstm.jld
 function main()
-	lstm = compile(:lstmnetwork)
+	lstm = compile(:lstm_network)
 	JLD.save("lstm.jld", "model", clean(lstm))
 end
 
