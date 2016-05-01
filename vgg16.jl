9a7e022be94b3a63c8ed3ea792a94a4a14db3bc8
kaandemiroz <odemiroz@ku.edu.tr>
2016-05-01 12:57:37 +0300

    VGG16 Complete
    Fixed the dimension mismatch error in the VGG16 model. Convolution
    layers are used for fully connected layers as well. wbf's replaced with
    cbf's

--- vgg16.jl
+++ vgg16.jl
@@ -31,10 +31,13 @@ end
 	conv5_2 = cbf(conv5_1; out = 512, cinit = map(Float64,weights[27]["weights"][1]), binit = reshape(map(Float64,weights[27]["weights"][2]), (1,1,512,1)), window = cwin, padding = pad, o...)
 	conv5_3 = cbf(conv5_2; out = 512, cinit = map(Float64,weights[29]["weights"][1]), binit = reshape(map(Float64,weights[29]["weights"][2]), (1,1,512,1)), window = cwin, padding = pad, o...)
 	pool5 = pool(conv5_3; pwindow = pwin, o...)
+	# VGG uses convolutional layers in fully connected as well
+	fc6 = cbf(pool5; out = 4096, winit = map(Float64, weights[32]["weights"][1]), binit = reshape(map(Float64, weights[32]["weights"][2]), (1,1,4096,1)), window = 7, o...)
 	# fc6 = wbf( pool5; out = 4096, winit = transpose( reshape(map(Float64,weights[32]["weights"][1]), (size(weights[32]["weights"][1],1)*size(weights[32]["weights"][1],2)*size(weights[32]["weights"][1],3), 4096) ) ), binit = reshape(map(Float64,weights[32]["weights"][2]), (1,1,4096,1)), f = :relu, o...)
-	# fc6 = drop(fc6; pdrop = pdrop, o...)
+	fc6 = drop(fc6; pdrop = pdrop, o...)
+	fc7 = cbf(fc6; out = 4096, winit = map(Float64, weights[34]["weights"][1]), binit = reshape(map(Float64, weights[34]["weights"][2]), (1,1,4096,1)), window = 1, o...)
 	# fc7 = wbf(fc6; out = 4096, winit = transpose( reshape(map(Float64,weights[34]["weights"][1]), (size(weights[34]["weights"][1],1)*size(weights[34]["weights"][1],2)*size(weights[34]["weights"][1],3), 4096) ) ), binit = reshape(map(Float64,weights[34]["weights"][2]), (1,1,4096,1)), f = :relu, o...)
-	return pool5
+	return fc7
 end
 
 # Compile the VGG-16 Model to vgg16.jld
